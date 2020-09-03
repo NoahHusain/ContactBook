cc68347047a7299689a192fc560859eb588916d0
NoahHusain <69663505+NoahHusain@users.noreply.github.com>
2020-09-02 22:16:01 -0500

    Initial Commit

---
+++ ContactBookClass.py
@@ -0,0 +1,33 @@
+# from ContactClass import Contact
+import mysql.connector
+
+
+class ContactBook:
+    def __init__(self):
+        self.contactList = []
+        self.mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="password",
+            database="contact_schema"
+        )
+        self.mycursor = self.mydb.cursor()
+
+    def createcontact(self, FirstName, LastName, Address, Number, Email):
+        sql = "INSERT INTO contact_book (first_name, last_name, address, number, email) VALUES (%s, %s, %s, %s, %s)"
+        val = (FirstName, LastName, Address, Number, Email)
+        self.mycursor.execute(sql, val)
+        self.mydb.commit()
+        print("1 Record Added to Contact Book")
+
+    def contactlookup(self, lookup):
+        query_string = "SELECT * FROM contact_book WHERE first_name = \'" + lookup + "\'"
+        self.mycursor.execute(query_string)
+        myresult = self.mycursor.fetchall()
+        return myresult
+
+    def deletecontact(self, contact_id):
+        sql = "DELETE FROM contact_book WHERE Id = " + str(contact_id)
+        self.mycursor.execute(sql)
+        self.mydb.commit()
+        print(self.mycursor.rowcount, "record(s) deleted")
---
+++ ContactBookMenu.py
@@ -0,0 +1,51 @@
+from ContactBookClass import ContactBook
+
+MyContactBook = ContactBook()
+
+
+def MainMenu():
+    main_menu_active = True
+    while main_menu_active == True:
+        welcome_message = input(
+            "Welcome to your address book! Would you like to add, find, or delete a contact (add/find/delete/close): ").lower()
+
+        if welcome_message == "add":
+            FirstName = input(
+                "What is the first name of the contact: ").lower()
+            LastName = input("What is the last name of the contact: ").lower()
+            Address = input("What is the address: ").lower()
+            Number = input("What is the phone number: ").lower()
+            Email = input("What is the email of the contact: ").lower()
+            MyContactBook.createcontact(
+                FirstName, LastName, Address, Number, Email)
+
+        elif welcome_message == "find":
+            fetchcontact = input(
+                "Please input the first name of the contact you wish to find: ").lower()
+            print(MyContactBook.contactlookup(fetchcontact))
+
+        elif welcome_message == "delete":
+            findcontact = input(
+                "What is the first name of the contact you wish to delete: ")
+            Contact = MyContactBook.contactlookup(findcontact)
+            for i in range(len(Contact)):
+                print("Poop", Contact[i][0], Contact[i][1], Contact[i]
+                      [2], Contact[i][3], Contact[i][4], Contact[i][5])
+
+            confirm_delete = input(
+                "Are you sure you wish to delete the above contact: ").lower()
+
+            if confirm_delete == 'yes':
+                MyContactBook.deletecontact(
+                    Contact[0][0])
+            else:
+                pass
+
+        elif welcome_message == "close":
+            main_menu_active = False
+
+        else:
+            print("We did not understand your input. Please try again")
+
+
+MainMenu()
